8fa7a3918cf2af3235e6196d50660024e1f3c578
Faith Jackson <undisputedfaith@gmail.com>
2025-01-10 10:52:20 +0100

    Fixed comment model and views

--- Amebor Zone/articles/views.py
+++ Amebor Zone/articles/views.py
@@ -1,40 +1,63 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from .models import Article
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from .models import Article, Comment  # Ensure Comment model is imported
+
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(LoginRequiredMixin, DetailView): 
+
+@method_decorator(csrf_protect, name='dispatch')
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        comment_text = request.POST.get("comment", "").strip()
+
+        if comment_text:
+            # Create a new comment and associate it with the article
+            Comment.objects.create(
+                article=self.object,
+                author=request.user,
+                text=comment_text
+            )
+        # Redirect back to the article detail page after adding the comment
+        return redirect('article_detail', pk=self.object.pk)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
-    def test_func(self): 
+    def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): 
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-    def test_func(self):  # Ensure correct indentation here
+    def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
+
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body',)    
+    fields = ('title', 'body',)
 
-    def form_valid(self, form):  # Ensure correct indentation here
+    def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
